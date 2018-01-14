e1ab66b94284360e6b774b526fbc0f2eacbd6486
Ruslan Shmelev <Ruslan.Shmelev@emc.com>
2018-01-14 22:40:19 +0300

    high level archecture

--- winline.py
+++ winline.py
@@ -1,3 +1,5 @@
+import time
+
 
 class Controller:
 
@@ -7,7 +9,12 @@ class Controller:
         pass
 
     def run(self):
-        pass
+        while(True):
+            data = self.export_data('some_urp')
+            pairs = self.data_analyzer(data)
+            if pairs:
+                self.telegram_connector(pairs)
+            time.sleep(self.DATA_EXPORT_TIMEOUT_SEC)
 
     def export_data(self, url):
         """
@@ -15,4 +22,22 @@ class Controller:
         :param url:
         :return: array of Data object
         """
+        return [url]
+
+    def data_analyzer(self, *args):
+        """
+        search same pairs from data array
+        :param args:
+        :return: pairs or None
+        """
+        return [args]
+
+    def telegram_connector(self, pairs):
+        """
+
+        :param pairs:
+        :return: 
+        """
         pass
+
+
