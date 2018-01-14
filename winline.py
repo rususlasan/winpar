7ef2680905faf39d3c4fbcf18bf7744f5a86bd8b
Ruslan Shmelev <Ruslan.Shmelev@emc.com>
2018-01-14 22:24:14 +0300

    start

---
+++ winline.py
@@ -0,0 +1,18 @@
+
+class Controller:
+
+    DATA_EXPORT_TIMEOUT_SEC = 60
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+    def export_data(self, url):
+        """
+
+        :param url:
+        :return: array of Data object
+        """
+        pass
