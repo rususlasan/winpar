f47eb2abca6b40fde6b1f072d568657fb425914d
Ruslan Shmelev <Ruslan.Shmelev@emc.com>
2018-01-15 19:52:30 +0300

    last attempt for test, while I am at work

--- telegram_pusher.py
+++ telegram_pusher.py
@@ -4,12 +4,12 @@ import telebot
 WINLINE_ALERT_BOT_TOKEN = '512466759:AAHQ-LqRYZ6KLxlHI4s0S9s1CnGJ8glyISk'
 CHANNEL_NAME = '@test_winline_alert'
 
+def start_bot(bot_to_start):
+    bot_to_start.polling(none_stop=True)
+
+
 bot = telebot.TeleBot(WINLINE_ALERT_BOT_TOKEN)
-
-
-def start_bot():
-    bot.polling(none_stop=True)
-
+start_bot(bot_to_start=bot)
 
 @bot.message_handler(commands=['alive'])
 def handle_alive_request(mes):
