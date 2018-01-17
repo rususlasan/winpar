fd107177708beca6e826fa52e2b3decab4440267
Ruslan Shmelev <Ruslan.Shmelev@emc.com>
2018-01-17 20:07:48 +0300

    added attempts in telegram send method

--- config.py
+++ config.py
@@ -4,9 +4,13 @@ import logging
 WINLINE_BOT_TOKEN = 'some token here'
 WINLINE_ALERT_CHANNEL_NAME = '@test_winline_alert'  # format - @CHANNEL_NAME
 
+# common telegram parameters
+SEND_MESSAGE_ATTEMPT_TIMEOUT_SEC = 10
+SEND_MESSAGE_ATTEMPT_MAX = 5
+
 # winline data scrapping parameters
 WINLINE_LIVE_URL = 'https://winline.ru/now/'
-DATA_EXPORT_TIMEOUT_SEC = 60
+DATA_EXPORT_TIMEOUT_SEC = 300
 
 # system settings
 PATH_TO_LOGS = '/var/log/winline.log'
--- telegram_pusher.py
+++ telegram_pusher.py
@@ -1,14 +1,28 @@
-import config
+import time
 import telebot
+
+import config
 
 
 bot = telebot.TeleBot(config.WINLINE_BOT_TOKEN)
 
 
 def post_message_in_channel(message):
-    try:
-        bot.send_message(chat_id=config.WINLINE_ALERT_CHANNEL_NAME, text='[BOT] %s' % message)
-    except:
-        config.logger.exception('Could not send message \n [{message}] in channel:'.format(message=message))
-
-
+    current_attempt = 1
+    config.logger.info('Try to send message: \n [{message}] in telegram...'.format(message=message))
+    while current_attempt <= config.SEND_MESSAGE_ATTEMPT_MAX:
+        try:
+            bot.send_message(chat_id=config.WINLINE_ALERT_CHANNEL_NAME, text='[BOT] %s' % message)
+            config.logger.info('Message send successfully.')
+            break
+        except Exception as e:
+            config.logger.error(
+                'Could not send message in channel: {exception}. Attempt {current_attempt} in {max_attempt}'
+                .format(message=message,
+                        exception=e,
+                        current_attempt=current_attempt,
+                        max_attempt=config.SEND_MESSAGE_ATTEMPT_MAX))
+            current_attempt += 1
+            time.sleep(config.SEND_MESSAGE_ATTEMPT_TIMEOUT_SEC)
+    else:
+        config.logger.error('Could not send message in 5 attempts!!!')
